a225a476d6893fe4db638a024f085ba41b2dc93e
Rajat Pathak <rajatinfy2008@gmail.com>
2020-01-27 23:42:28 +0530

    caching changes

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,21 +3,50 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+const NodeCache = require('node-cache');
+const Request = require('request');
+import { environment } from './environments/environment';
+
+const appCache = new NodeCache({ stdTTL: 300, checkperiod: 120 });
 
 const init = async () => {
   const server = new Server({
     port: 3333,
-    host: 'localhost'
+    host: 'localhost',
+    routes:{
+      cors: true
+    }
   });
+
+  function fetchQuote(url: string) {
+    return new Promise((resolve, reject) => {
+      Request(url, (error, res, body) => {
+        if (error) reject(error);
+        if (res) {
+          if (res['statusCode'] !== 200)
+            return res;
+        }
+        resolve(body);
+      });
+    });
+  }
 
   server.route({
     method: 'GET',
-    path: '/',
+    path: '/beta/stock/{symbol}/chart/{period}',
     handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
-    }
+      const { symbol, period } = request.params;
+      const apiKey = request.query.token;
+      const urlString = environment.apiUrl + '/beta/stock/' + symbol + '/chart/' + period + '?token=' + apiKey;
+
+      const cachedResponse = appCache.get( urlString );
+      if(cachedResponse) return cachedResponse;
+
+      return fetchQuote(urlString).then(response => {
+        appCache.set(urlString, response, 10000);
+        return response;
+      });
+    }    
   });
 
   await server.start();
